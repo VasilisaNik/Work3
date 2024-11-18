26c153f932aa7e9b902d5ec71d2f424bc848fc07
VasilisaNik <vslnikonova@gmail.com>
2024-11-18 15:12:44 +0500

    Создан каркас

--- main.cpp
+++ main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
+void Fun1(const string& filename, vector<string>& vect) //Чтение строк в вектор
+{
+
+}
+
+void Fun2(vector<string>& vect) //Дополнительная обработка данных
+{
+
+}
+
+void Fun3(const string& filename, vector<string>& vect) //Запись в файл
+{
+
+}
+
+int Fun4(vector<string>& lvect) //Подсчет количества строк в векторе
+{
+
+}
+
+void Fun5(vector<string>& vect) //Вывод строк на экран
+{
+
+}
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    setlocale(LC_ALL, "RUS");
+    //string filename1 = "Text1.txt"
+    vector<string> vect;
+    Fun1("Text1.txt", vect);
+    Fun5(vect);
+    Fun3("Text2.txt", vect);
+    Fun2(vect);
+
+    int lineCount = Fun4(vect);
+    wcout << L"Количество строк: " << lineCount << endl;
+
     return 0;
+
 }
